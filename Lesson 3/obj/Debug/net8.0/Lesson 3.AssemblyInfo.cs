5aeb936b3a600b07415b732a494d928d5f2ee1ca
Andrey <androsdm@gmail.com>
2023-12-02 21:21:01 +0300

    убрал неиспользуемую переменную

--- Lesson 3/obj/Debug/net8.0/Lesson 3.AssemblyInfo.cs
+++ Lesson 3/obj/Debug/net8.0/Lesson 3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson 3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0facda6c8797881cbe99d62848badae001f8c3a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson 3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson 3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
